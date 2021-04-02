e920c07de9c1c5e15be4ed5cdf555a007dca6668
MMJ9703 <64726856+MMJ9703@users.noreply.github.com>
2021-04-02 12:05:02 +0800

    Update read_mysql.py

--- read_mysql.py
+++ read_mysql.py
@@ -16,6 +16,8 @@ def read_mysql(sqlEngine,ID,col,starttime,endtime):
 def read_mysql_pre(sqlEngine,ID,col,starttime,endtime):
     data_lst = {}
     starttime = str(pd.to_datetime(starttime)-datetime.timedelta(hours=2))
+    print('测试数据开始时间:'+starttime)
+    print('测试数据结束时间:'+endtime)
     for i in col:
         table_name = 'id'+ID+'pt_'+i.lower()
         data = pd.read_sql("select * from  "+table_name+" WHERE `Time` between '%s' and '%s'"%(starttime,endtime) , con=sqlEngine, index_col=None)
